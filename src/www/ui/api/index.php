5ef98b7c3c24f230ec58413c37b29929514303c6
Max Wittig <max.wittig@siemens.com>
2017-06-13 15:20:52 +0200

    fix url schema for api request

--- src/www/ui/api/index.php
+++ src/www/ui/api/index.php
@@ -22,59 +22,35 @@ use \www\ui\api\helper\DbHelper;
 $app = new Silex\Application();
 $app['debug'] = true;
 
-$app->PUT('/', function (Application $app, Request $request)
+$app->GET('/repo/api/v1/organize/uploads/{id}', function (Application $app, Request $request, $id)
 {
   $restHelper = new RestHelper();
-  $filteredFile = $restHelper->getFilteredFile($request->getContent());
-  return new Response($filteredFile->getJSON());
-});
-
-$app->DELETE('/', function (Application $app, Request $request)
-{
-  $restHelper = new RestHelper();
-  $dbHelper = new DbHelper();
-  $id = 3;
-  if($dbHelper->doesUploadIdExist($id))
+  //get the id from the fossology user
+  if(is_integer($id))
   {
-    define("PLUGIN_DB_ADMIN", 0);
-    TryToDelete($id, $restHelper->getUserId(), $restHelper->getGroupId(), $restHelper->getUploadDao());
-    $info = new Info(202, "Delete Job for file with id ". $id,InfoType::INFO);
-    return new Response($info->getJSON(), $info->getCode());
+    return new Response(json_encode($restHelper->getFolderHelper()->getUploads($restHelper->getUserId(), $id), JSON_PRETTY_PRINT));
   }
   else
   {
-    $error = new Info(404,"Id ".$id." doesn't exist", InfoType::ERROR);
-    return new Response($error->getJSON(), $error->getCode());
+    $error = new Info(400, "Bad Request. $id is not a number!", InfoType::ERROR);
+    return new Response($error->getJSON());
   }
+
 });
 
-$app->GET('/', function (Application $app, Request $request)
-{
-  //get the id from the fossology user
-  $restHelper = new RestHelper();
-  return new Response(json_encode($restHelper->getFolderHelper()->getUploads($restHelper->getUserId()), JSON_PRETTY_PRINT));
-});
-
-$app->GET('/v1/organize/uploads/{id}', function (Application $app, Request $request, $id)
-{
-  $restHelper = new RestHelper();
-  //get the id from the fossology user
-  return new Response(json_encode($restHelper->getFolderHelper()->getUploads($restHelper->getUserId(), $id), JSON_PRETTY_PRINT));
-});
-
-$app->PATCH('/v1/organize/uploads/{id}', function (Application $app, Request $request, $id)
+$app->PATCH('/repo/api/v1/organize/uploads/{id}', function (Application $app, Request $request, $id)
 {
   return new Response('How about implementing organizeUploadsIdPatch as a PATCH method ?');
 });
 
-$app->PUT('/v1/organize/uploads/', function (Application $app, Request $request)
+$app->PUT('/repo/api/v1/organize/uploads/', function (Application $app, Request $request)
 {
   $restHelper = new RestHelper();
   $filteredFile = $restHelper->getFilteredFile($request->getContent());
   return new Response($filteredFile->getJSON());
 });
 
-$app->GET('/v1/organize/uploads', function (Application $app, Request $request)
+$app->GET('/repo/api/v1/organize/uploads', function (Application $app, Request $request)
 {
   $restHelper = new RestHelper();
   //get the id from the fossology user
@@ -82,22 +58,23 @@ $app->GET('/v1/organize/uploads', function (Application $app, Request $request)
 });
 
 
-$app->DELETE('/v1/organize/uploads/{id}', function (Application $app, Request $request, $id)
+$app->DELETE('/repo/api/v1/organize/uploads/{id}', function (Application $app, Request $request, $id)
 {
   $restHelper = new RestHelper();
   $dbHelper = new DbHelper();
-  if($dbHelper->doesUploadIdExist($id))
+
+  if ($dbHelper->doesUploadIdExist($id))
   {
-    define("PLUGIN_DB_ADMIN", 0);
     TryToDelete($id, $restHelper->getUserId(), $restHelper->getGroupId(), $restHelper->getUploadDao());
-    $info = new Info(202, "Delete Job for file with id ". $id,InfoType::INFO);
+    $info = new Info(202, "Delete Job for file with id " . $id, InfoType::INFO);
     return new Response($info->getJSON(), $info->getCode());
   }
   else
   {
-    $error = new Info(404,"Id ".$id." doesn't exist", InfoType::ERROR);
+    $error = new Info(404, "Id " . $id . " doesn't exist", InfoType::ERROR);
     return new Response($error->getJSON(), $error->getCode());
   }
+
 });
 
 $app->run();
