35cdcfacdfe026db48dcd3c0174e5511c3fda4a4
Max Wittig <max.wittig@siemens.com>
2017-07-03 17:32:21 +0200

    Update check-changelog.sh

--- src/testing/travis/check-changelog.sh
+++ src/testing/travis/check-changelog.sh
@@ -16,5 +16,5 @@ curl https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm install v6
-npm install --save-dev conventional-changelog-lint
+npm install conventional-changelog-lint
 node_modules/conventional-changelog-lint/distribution/cli.js --from=master
